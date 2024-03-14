1e31170de8089caf3bfce981bb6273c7e71833c4
AndeersonDEV <andeerson_sps@hotmail.com>
2024-03-14 13:45:12 -0300

    fecth: inserindo grade de setor

---
+++ app/main.js
@@ -0,0 +1,21 @@
+// const btnAvalara = document.querySelector('.avalara')
+// const btnMotorista = document.querySelector('.motoristas')
+// const btnServiceNow = document.querySelector('.servicenow')
+// const iframe = document.querySelector('.iframe')
+
+
+// const urlAvalara = 'file://1965dep01939848/Users/4351460/Desktop/Painel%20Avalara/Geral1.html'
+// const urlMotorista = 'file://1965dep01939848/Users/4351460/Desktop/Painel%20Motoristas/index.html'
+// const urlServiceNow = 'https://gpabr.service-now.com/nav_to.do?uri=%2F$pa_dashboard.do%3Fsysparm_dashboard%3D806bf3201bccb954ed7a53dc4b4bcbf0%26sysparm_tab%3D7a7bff201bccb954ed7a53dc4b4bcbd6%26sysparm_cancelable%3Dtrue%26sysparm_editable%3Dundefined%26sysparm_active_panel%3Dfalse'
+
+
+// btnAvalara.addEventListener('click',() =>{
+//     iframe.setAttribute('src',urlAvalara)
+// }) 
+
+// btnMotorista.addEventListener('click',() =>{
+//     iframe.setAttribute('src',urlMotorista)
+// }) 
+// btnServiceNow.addEventListener('click',() =>{
+//     iframe.setAttribute('src',urlServiceNow)
+// }) 
