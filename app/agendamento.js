3c17f48684b2fefa68354bad518696ab12ca8a16
AndeersonDEV <andeerson_sps@hotmail.com>
2024-04-16 08:31:38 -0300

    fecth: inclusao de telas

---
+++ app/agendamento.js
@@ -0,0 +1,25 @@
+const painelGpo = document.querySelector('.painelGeralGpo')
+const painelCentralizados = documente.querySelector('.painelCentralizados')
+const painelPerformance = document.querySelector('.painelPerformance')
+const btnGPo = document.querySelector('.btnGpo')
+const btnIndicadores = document.querySelector('.btnIndGPo')
+const btnPerformance = document.querySelector('.btnPerf')
+
+btnGPo.addEventListener('click', ()=>{
+    painelGpo.classList.remove('ativaGpo')
+    painelCentralizados.classList.add('ativaGpo')
+    painelPerformance.classList.add('ativaGpo')
+})
+
+btnIndicadores.addEventListener('click', ()=>{
+    painelGpo.classList.add('ativaGpo')
+    painelCentralizados.classList.remove('ativaGpo')
+    painelPerformance.classList.add('ativaGpo')
+})
+
+btnPerformance.addEventListener('click', ()=>{
+    painelGpo.classList.add('ativaGpo')
+    painelCentralizados.classList.add('ativaGpo')
+    painelPerformance.classList.remove('ativaGpo')
+})
+
